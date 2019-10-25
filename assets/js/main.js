3bd6d8d846884e446e54051d98b8b1bbb8ea468c
Hannah Guyer <hannah.guyer@gmail.com>
2019-10-25 11:41:23 +1100

    update direct

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+$(document) .ready(function(){
+	console.log("The page is ready!");
+
+// toggle large class on research item
+
+	$('.research-item') .click(function(){
+		$(this).toggleClass('large');
+	});
+
+// toggle active class on nav item
+
+	$('.nav-item').click(function(){
+		$('.nav-item').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	var hideAll = function() {
+		$('.research-item').addClass('hide');
+	}
+
+// button filters
+
+	$('.btn-art)').click(function(){
+		hideAll();
+		$('.art').removeClass('hide');
+	});
+
+	$('.btn-sculpture').click(function(){
+		hideAll();
+		$('.sculpture').removeClass('hide');
+	});
+
+	$('.btn-performance').click(function(){
+		hideAll();
+		$('.btn-performance').removeClass('hide');
+	});
+
+	$('.btn-music').click(function(){
+		hideAll();
+		$('.btn-music').removeClass('hide');
+	});
+	
+// button all
+
+	$('.btn-all').click(function(){
+		$('.research-item').removeClass('hide');
+	});
+
+});
